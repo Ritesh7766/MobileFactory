1219fefd79cef76a3c42817554fdc003977e9440
Ritesh Saha <riteshsaha214@gmail.com>
2023-08-30 20:39:56 +0530

    [ added additional tests ]

--- tests/test_order_endpoints.py
+++ tests/test_order_endpoints.py
@@ -33,6 +33,28 @@ class TestOrderEndpoint(unittest.TestCase):
         self.assertEqual(total, data["total"])
 
 
+    def check_invalid_codes(self, data):
+        for code in data['Invalid Codes']:
+            self.assertNotIn(code, PARTS)
+
+
+    def check_missing_components(self, parts, data):
+        specified_component_types = set([PARTS[code]['type'] for code in parts])
+        missing_components = {'Screen', 'Camera', 'OS', 'Port', 'Body'} - specified_component_types
+        self.assertEqual(missing_components, set(data['Missing Components']))
+
+    
+    def check_repeated_components(self, parts, data):
+        specified_component_types, repeated_component_types = set(), set()
+        for code in parts:
+            part = PARTS[code]
+
+            if part['type'] in specified_component_types:
+                repeated_component_types.add(part["type"])
+            specified_component_types.add(part['type'])
+        self.assertEqual(repeated_component_types, set(data['Repeated Component Types']) )
+
+
     def test_all_combinations(self):
         order_ids = set()
         # Try every possible combination of requests.
@@ -51,12 +73,17 @@ class TestOrderEndpoint(unittest.TestCase):
                     continue
 
                 # Check bad requests
+
                 self.assertEqual(response.status_code, 400)
+                if 'Invalid Request' in data:
+                    self.assertGreater(len(parts), 5)
+
+                if 'Missing Components' in data:
+                    self.check_missing_components(parts=parts, data=data)
+
+                if 'Repeated Component Types' in data:
+                    self.check_repeated_components(parts=parts, data=data)
                 
-
-
-
-
 
 if __name__ == "__main__":
     unittest.main()
